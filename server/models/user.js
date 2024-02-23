2b7d08002945ec833a581e54fb6466d0831908f9
Kronkan <h22thsta@du.se>
2024-02-23 10:16:08 +0100

    Co-authored-by: Filip Lindgren <Filiplindgren@users.noreply.github.com>

---
+++ server/models/user.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+
+    }, 
+    { underscored: true }
+    );
+};
